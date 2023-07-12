1ddd6037d833285f5a0625995c619f3eb9ce302c
Tatenda <tatenda@tradingeconomics.com>
2023-07-12 19:22:52 +0200

    v2.2.7

--- tradingeconomics/calendar.js
+++ tradingeconomics/calendar.js
@@ -14,6 +14,7 @@ global.importance = null;
 global.group = null;
 global.values = null;
 
+
 /***************************************************************************************************************  
     ### Without parameters a list of all events will be provided
     ##### parameters:  
